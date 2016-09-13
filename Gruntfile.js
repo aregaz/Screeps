f2b1024f5b5074abc30a9db7b3a32de04d55ad41
Illia Ratkevych <illia.ratkevych@gmail.com>
2016-09-13 23:02:22 +0300

    Update gruntfile with screeps task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,18 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-screeps');
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        screeps: {
+            options: {
+                email: 'illia.ratkevych@gmail.com',
+                password: 'mypassword',
+                branch: 'default',
+                ptr: false
+            },
+            dist: {
+                src: ['src/*.js']
+            }
+        }
     });
 };
