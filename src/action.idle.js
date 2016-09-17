6b486ee4ab3dce29367b0296697e379d95d19a19
Illia Ratkevych <illia.ratkevych@gmail.com>
2016-09-17 10:36:53 +0300

    Update scripts from online.

--- src/action.idle.js
+++ src/action.idle.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('action.idle');
- * mod.thing == 'a thing'; // true
- */
-
 module.exports = {
     run: function(creep, coordinates, stopIdleCondition, afterAction) {
         if (creep.memory.action != 'idle') return;
